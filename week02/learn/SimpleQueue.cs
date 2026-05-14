7cf616e77732cda0457cc17dc8dd773fe831a448
anjuroca85 <anjuroca85@gmail.com>
2026-05-14 13:57:31 +0200

    Complet Task1 in week2 for learning

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -37,7 +37,9 @@
         // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
-        try {
+
+        try
+        {
             queue.Dequeue();
             Console.WriteLine("Oops ... This shouldn't have worked.");
         }
@@ -54,7 +56,8 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        // _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +69,10 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        // var value = _queue[1];
+        var value = _queue[0];
+        // _queue.RemoveAt(1);
+        _queue.RemoveAt(0);
         return value;
     }
 }

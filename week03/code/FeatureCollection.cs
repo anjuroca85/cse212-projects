37de56bb7307154e1f154f7e1d1d9b1046074fa1
anjuroca85 <anjuroca85@gmail.com>
2026-05-23 23:59:27 +0200

    W3 sets and maps EOW

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -2,4 +2,16 @@ public class FeatureCollection
 {
     // TODO Problem 5 - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    //A feature  map with its properties like place and magnitude.
+    public List<Feature> Features {get; set;} = [];
+}
+public class Feature
+{
+    public EarthquakeProperties Properties {get; set;} = new();
+}
+
+public class EarthquakeProperties
+{
+    public string Place {get; set;} = "";
+    public double Mag {get; set;}
 }
